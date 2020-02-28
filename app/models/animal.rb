afa0c314c21fd8a531fd14ea6529968903534a5b
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2020-02-28 13:46:06 +0100

    Edit and delete added for animals

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,5 +1,6 @@
 class Animal < ApplicationRecord
   belongs_to :shelter
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
   validates :name, presence: true
   validates :animal_type, presence: true
